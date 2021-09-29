5fa2c1c2bed0516f50ff6f1f5e7898b6a3e6f618
PauloBaronista <paulo@rpm.com.br>
2021-09-28 21:28:15 -0300

    Trocando link Zap

--- V2/application/views/html_footer.php
+++ V2/application/views/html_footer.php
@@ -11,7 +11,7 @@ echo script_tag('assets/js/bootstrap.min.js');
     <a href="https://online.crm.anapro.com.br/WebCRMService/Pages/chat/cliente/v2/ChatClienteEntrada.aspx?conta=vTjaeIT3TpI1&keyIntegradora=29F2D83A-29CF-4359-B1AC-40CE6FA19E70&keyAgencia=29F2D83A-29CF-4359-B1AC-40CE6FA19E70&strDir=coelhodafonseca&campanha=nOhDjNO4xhg1&canal=Q2yXd9MtoHA1&produto=GTsXeOdjI1E1&strmidia=Hotsite+Empreendimento&strpeca=&usuarioEmail=&strgrupopeca=&strcampanhapeca=&nome=&email=&telefoneDDD=&telefone=&strTexto=&keyexterno=&urlep=&urlcp=&urlca=&urlat=&strMostrarTopo=true&strAutoSubmit=true&strUsarDadosAnteriores=true&emailobrigatorio=true&telefoneobrigatorio=false&texto=" class="chat" target="_blank">Chat on-line</a>
     <a href="#" class="phone">(11) 4412-2692</a>
     <!-- <a href="#contato" class="mail">Enviar mensagem</a> -->
-    <a href="https://api.whatsapp.com/send?1=pt_BR&phone=+5511930352284" target="_blank" class="whats">Whatsapp</a>
+    <a href="https://online.crm.anapro.com.br/WebCRMService/Pages/chat/cliente/ChatClienteWhatsAppEntrada.aspx?conta=vTjaeIT3TpI1&keyIntegradora=29F2D83A-29CF-4359-B1AC-40CE6FA19E70&keyAgencia=29F2D83A-29CF-4359-B1AC-40CE6FA19E70&strDir=coelhodafonseca&campanha=nOhDjNO4xhg1&canal=9B19BZMXEwI1&produto=GTsXeOdjI1E1&strmidia=Hotsite+Empreendimento&strpeca=&usuarioEmail=&strgrupopeca=&strcampanhapeca=&nome=&email=&telefoneDDD=&telefone=&strTexto=&keyexterno=&urlep=&urlcp=&urlca=&urlat=&strMostrarTopo=true&strAutoSubmit=true&strUsarDadosAnteriores=true&emailobrigatorio=true&telefoneobrigatorio=false&texto=" target="_blank" class="whats">Whatsapp</a>
 </div>
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
